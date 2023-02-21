add6109da74cdde07891fb83f17978ea8c540d59
BenedictKagwa4040 <kagwabenedict8@gmail.com>
2023-02-21 09:52:45 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * main - prints _putchar and a new line
+ * main -Entry point
  * Return: Always 0
  */
 int main(void)
+
 {
-	_Putchar('_');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -15,7 +16,8 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-return (0);
+	return (0);
 }
+
 
 
