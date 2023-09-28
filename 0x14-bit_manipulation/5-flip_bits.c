173c46385722831326cb4e206ea5d7e220304b9f
BenedictKagwa4040 <kagwabenedict8@gmail.com>
2023-09-28 05:32:23 -0700

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*flip_bits - counts the number of bits
+*@n: the bit
+*@m: the index
+*Return: if an error occurs
+*/
+
+
+unsigned int pli_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int xor = n ^ m, bits = 0;
+
+while (xor > 0)
+{
+bits += (xor & 1);
+xor >>= 1;
+}
+return (bits);
+}
+
